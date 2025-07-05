56ab2e712af0ef55e929fd690efd57ba22adfd7e
Edmir Cardoso <edmircrds@gmail.com>
2025-07-05 17:41:40 -0100

    Clear projeto

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo "Hello World!";
-});
-
-Route::get('/about', function(){
-    echo "About us";
-
-});
-Route::get('/testebranch', function(){
-    echo "Testando branch";
-
-});

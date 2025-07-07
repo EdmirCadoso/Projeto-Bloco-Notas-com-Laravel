c31160d865e0868cd5335c900a7f5f68b08f1097
Edmir Cardoso <edmircrds@gmail.com>
2025-07-07 15:49:16 -0100

    Validation formulario

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,6 +13,33 @@ class AuthController extends Controller
 
     public function logout()
     {
-        echo 'logount';
+        echo 'logout';
+    }
+
+     public function loginSubmit(Request $request)
+    {
+
+        //form validation
+        $request -> validate(
+            //rules
+            [
+                'text_username' => 'required|email',
+                'text_password' => 'required|min:6|max:16',
+            ],
+
+            //error mensages
+            [
+                'text_username.required' => 'O username é obrigatório',
+                'text_username.email' => 'Username deve ser um email válido ',
+                'text_password.required' => 'A password é obrigatória',
+                'text_password.min' => 'A password deve ter pelo menos :min caracteres',
+                'text_password.max' => 'A password deve ter no máximo :max caracteres',
+            ]
+        );
+
+        $username = $request ->input('text_username');
+        $password = $request ->input('text_password');
+
+        echo "ok!";
     }
 }
